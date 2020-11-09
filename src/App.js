4e3861c30b138a12424ccf1230feed783f5c54ae
Yoga Nugraha Pryandika <yogapryandika@gmail.com>
2020-11-09 20:34:06 +0700

    added front end for showing technisians and add technisians

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Fragment, useEffect } from 'react';
 
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -12,10 +17,16 @@ const App = () => {
     // init materialize JS
     M.AutoInit();
   });
+
   return (
     <Fragment>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
