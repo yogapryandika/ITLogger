99a5dc890635f10f41f8b2720758824cc7677d97
Yoga Nugraha Pryandika <yogapryandika@gmail.com>
2020-11-10 11:56:01 +0700

    added Redux for add logs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
@@ -19,17 +21,19 @@ const App = () => {
   });
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
